ba1994ca9a96f7978470afc8320613274216dc3a
emilyzugel <zugel21@proton.me>
2025-11-07 00:17:54 +0000

    finished frontend signin and connected to server mongodb

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,80 @@
+import { useEffect, useRef, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash'
+import api from '../../services/api.js'
 import './style.css'
 
-export function Home() {
+function Home() {
+
+  // Save inputs values with useRef from react
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+  // const inputPassword = useRef()
+
+  // Get users data from DB
+  const [users, setUsers] = useState([])
+
+  async function getUsers() {
+    try {
+      const response = await api.get('/users')
+      console.log('API Response:', response.data)
+      setUsers(response.data.data)
+    } catch (error) {
+      console.error('Error fetching users:', error)
+    }
+  }
+  //
+
+  // Get users from db on page load
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+
+  // Create users
+  async function createUser() {
+    await api.post('/users', {
+      name: inputName.current.value,
+      age: inputAge.current.value,
+      email: inputEmail.current.value
+    })
+    getUsers()
+  }
+
+  //Delete User
+  async function deleteUser(id) {
+    await api.delete(`/users/${id}`)
+    getUsers()
+  }
 
   return (
     <main>
       <div className='register background'>
         <h2>Sign In</h2>
-
         <form>
-          <input type='text' name='name' placeholder='ex: John Smith' />
-          <input type='email' name='email' placeholder='ex: email@email.com' />
-          <input type='number' name='age' placeholder='ex: 20' />
-          <button type="submit">Register</button>
+          <input ref={inputName} type='text' name='name' placeholder='ex: John Smith' />
+          <input ref={inputEmail} type='email' name='email' placeholder='ex: email@email.com' />
+          <input ref={inputAge} type='number' name='age' placeholder='ex: 20' />
+          <button onClick={createUser} type="button">Register</button>
         </form>
       </div>
+
       <div className='users background'>
-        <div className='user-card'>
-          <div className='user-content'>
-            <p>Name:</p>
-            <p>Email:</p>
+        {users.slice().reverse().map(user => (
+          <div className='user-card' key={user.id}>
+            <div key={user.id} className='user-content'>
+              <p>Name: {user.name}</p>
+              <p>Email: {user.email}</p>
+            </div>
+            <button onClick={() => deleteUser(user._id)} type='button'>
+              <FontAwesomeIcon icon={faTrash} style={{ color: "rgb(66, 73,128)" }} />
+            </button>
           </div>
-          <button>
-            <i class="fa-solid fa-trash" style="color: #ffffff;"></i>          </button>
-        </div>
+        ))}
       </div>
     </main>
   )
 }
+
+export default Home;
